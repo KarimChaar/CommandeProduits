2d40fafe4991fa3f454ebf493621926cdea08d44
KarimChaar <karimchaar09@gmail.com>
2022-05-17 17:56:07 +0100

    i added Client model

---
+++ Models/Client.cs
@@ -0,0 +1,30 @@
+﻿namespace CommandeProduct.Models
+{
+    public enum type { professionnel, particulier };
+    public struct adresse
+    {
+        private string rue;
+        private string ville;
+        private string pays;
+    }
+
+    public class Client
+    {
+        private string nom;
+
+        private adresse location;
+
+        private type type_C;
+
+        public Client(string nom, adresse location, type type_C)
+        {
+            this.nom = nom;
+            this.location = location;
+            this.type_C = type_C;
+        }
+
+
+
+        
+    }
+}
