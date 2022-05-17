8e2d328b8cf2c241f20cd84a8c57d17241fa0c94
Youssef1998d <youssefdrira32@gmail.com>
2022-05-17 17:06:45 +0100

    YD_1_added_Produits_file

---
+++ Models/Produit.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandeProduct.Models
+{
+
+    public class Produit
+    {
+        private string nomP;
+
+        public Produit(string nomP)
+        {
+            this.nomP = nomP;
+        }
+
+
+
+        
+    }
+}
